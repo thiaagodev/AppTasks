0464afac0bc56f4338ea0c656fbd46b7e294cb9a
Thiago Ferreira <thiaago.dev@gmail.com>
2022-10-23 15:02:54 -0300

    save login data in shared preferences

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -4,14 +4,17 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.model.ValidationModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
+import com.devmasterteam.tasks.service.repository.SecurityPreferences
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val personRepository = PersonRepository(application.applicationContext)
+    private val securityPreferences = SecurityPreferences(application.applicationContext)
     private val _validation = MutableLiveData<ValidationModel>()
 
     val login: LiveData<ValidationModel> = _validation
@@ -21,8 +24,12 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
      * Faz login usando API
      */
     fun doLogin(email: String, password: String) {
-        personRepository.login(email, password, object: APIListener<PersonModel> {
+        personRepository.login(email, password, object : APIListener<PersonModel> {
             override fun onSucess(model: PersonModel) {
+                securityPreferences.store(TaskConstants.SHARED.PERSON_KEY, model.personKey)
+                securityPreferences.store(TaskConstants.SHARED.TOKEN_KEY, model.token)
+                securityPreferences.store(TaskConstants.SHARED.PERSON_NAME, model.name)
+
                 _validation.value = ValidationModel()
             }
 
