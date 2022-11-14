eb97496547a9df96f2e493ac251169b83826b0f4
Thiago Ferreira <thiaago.dev@gmail.com>
2022-11-14 11:43:55 -0300

    melhoria no base repository

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PriorityRepository.kt
@@ -12,7 +12,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class PriorityRepository(private val context: Context) : BaseRepository() {
+class PriorityRepository(context: Context) : BaseRepository(context) {
 
     private val remote = RetrofitClient.getService(PriorityService::class.java)
     private val local = TaskDatabase.getDatabase(context).priorityDAO()
@@ -28,7 +28,7 @@ class PriorityRepository(private val context: Context) : BaseRepository() {
     }
 
     fun getDescription(id: Int): String {
-        var cached = PriorityRepository.getDescription(id)
+        val cached = PriorityRepository.getDescription(id)
 
         return if (cached == "") {
             val description = local.getDescription(id)
@@ -40,20 +40,7 @@ class PriorityRepository(private val context: Context) : BaseRepository() {
     }
 
     fun list(listener: APIListener<List<PriorityModel>>) {
-        val call = remote.list()
-        call.enqueue(object : Callback<List<PriorityModel>> {
-            override fun onResponse(
-                call: Call<List<PriorityModel>>,
-                response: Response<List<PriorityModel>>
-            ) {
-                handleResponse(response, listener)
-            }
-
-            override fun onFailure(call: Call<List<PriorityModel>>, t: Throwable) {
-                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
-            }
-
-        })
+        executeCall(remote.list(), listener)
     }
 
     fun list(): List<PriorityModel> {
