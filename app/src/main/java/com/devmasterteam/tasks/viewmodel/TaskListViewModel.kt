b4a3951bb49e5c3e5487be5e7d4be1f9452220b4
Thiago Ferreira <thiaago.dev@gmail.com>
2022-11-14 11:33:34 -0300

    remoção de tarefas

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskListViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskListViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.TaskModel
+import com.devmasterteam.tasks.service.model.ValidationModel
 import com.devmasterteam.tasks.service.repository.PriorityRepository
 import com.devmasterteam.tasks.service.repository.TaskRepository
 
@@ -16,8 +17,10 @@ class TaskListViewModel(application: Application) : AndroidViewModel(application
     private val priorityRepository = PriorityRepository(application.applicationContext)
 
     private val _tasks = MutableLiveData<List<TaskModel>>()
+    private val _delete = MutableLiveData<ValidationModel>()
 
     val tasks: LiveData<List<TaskModel>> = _tasks
+    val delete: LiveData<ValidationModel> = _delete
 
     fun list() {
         taskRepository.list(object : APIListener<List<TaskModel>> {
@@ -30,6 +33,19 @@ class TaskListViewModel(application: Application) : AndroidViewModel(application
 
             override fun onFailure(message: String) {
 
+            }
+
+        })
+    }
+
+    fun delete(id: Int) {
+        taskRepository.delete(id, object : APIListener<Boolean> {
+            override fun onSucess(model: Boolean) {
+                list()
+            }
+
+            override fun onFailure(message: String) {
+                _delete.value = ValidationModel(message)
             }
 
         })
